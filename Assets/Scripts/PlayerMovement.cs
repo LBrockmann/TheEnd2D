54005200ec8359be0fbec50cc53436870d31e4a3
Luke Brockmann <lb3297@nyu.edu>
2019-09-15 14:54:47 -0400

    Movement fixed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rigbod;
+
+    public float speed;
+    public float maxVelocity;
+    public float jumpforce;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +19,30 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        rigbod.velocity = Vector3.ClampMagnitude(rigbod.velocity, maxVelocity);
+        
+        if (Input.GetKey(KeyCode.D))
         {
-            
+            rigbod.velocity = new Vector2(speed, rigbod.velocity.y);
         }
-            
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigbod.velocity = new Vector2(-speed, rigbod.velocity.y);
+        }
+
+        /*else
+        {
+            rigbod.velocity = new Vector2(0f, rigbod.velocity.y);
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigbod.AddForce(new Vector2(0,jumpforce), ForceMode2D.Impulse);
+            Debug.Log("Jump");
+        }
+
+        
+
     }
 }
